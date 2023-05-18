58ad7f855e1f1b79a8c84857ff10a9b16cbaa870
Travis Donia <tdonia@gmail.com>
2023-05-18 10:51:58 -0400

    hello, model model

---
+++ ModelModel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ModelModel = () => {
+  return (
+    <div>
+      {/* Your component JSX */}
+      <h1>Hello, ModelModel!</h1>
+    </div>
+  );
+};
+
+export default ModelModel;
