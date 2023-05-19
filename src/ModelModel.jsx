6306854cacbacd46ef3f24057fd34fa2e59a658a
Travis Donia <tdonia@gmail.com>
2023-05-19 15:57:47 -0400

    cleaner

--- src/ModelModel.jsx
+++ src/ModelModel.jsx
@@ -5,37 +5,37 @@ import './tailwind.css';
 const ModelModel = () => {
 
   const [show, setShow] = useState('', '')
-  const [model] = useState('');
+  const [model, setModel] = useState('');
 
   const showModal = (e) => {
-    console.log("model")
-    console.log(model)
+    const modelClick = e.target.getAttribute("data-model");
+    if (!!modelClick) {
+      console.log("modelClick: "+ modelClick)
+      setModel(modelClick);
+      console.log(model)
+    }
     setShow(!show);
   };
 
 
-  if (model === "") {
-    return (
-      <div>
-        <SetupCTA onClickHandler={showModal} />
-        <Modal onClose={showModal} show={show}/>
-      </div>
-    );
-  }else{
-    return (
-      <div>
-        <button onClick={showModal} className="items-center justify-center px-4 py-2 bg-white hover:bg-white-600 rounded-lg focus:outline-none border border-solid border-neutral-400">
-          GPT-3 <span className="text-teal-500">///</span>
-        </button>
-        <Modal onClose={showModal} show={show}/>
-      </div>
-    );
-  }
+  return (
+    <div>
+    { model == "" ? ( 
+      <SetupCTA onClick={showModal} />
+      ) : (
+      <button onClick={showModal} className="items-center justify-center px-4 py-2 bg-white hover:bg-white-600 rounded-lg focus:outline-none border border-solid border-neutral-400">
+        {model} <span className="text-teal-500">///</span>
+      </button>
+    )}
+      <Modal model={model} onClose={showModal} show={show}/>
+    </div>
+  );
+  
 };
 
-const SetupCTA = ({onClickHandler}) => {
+const SetupCTA = ({onClick}) => {
  return (
-  <button onClick={onClickHandler} className="items-center justify-center px-4 py-2 bg-white hover:bg-white-600 rounded-lg focus:outline-none border border-solid border-red-400">
+  <button onClick={onClick} className="items-center justify-center px-4 py-2 bg-white hover:bg-white-600 rounded-lg focus:outline-none border border-solid border-red-400">
     Add API Key <span className="text-red-500">///</span>
   </button>
  )
