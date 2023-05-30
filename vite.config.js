1804c794559ab5fc35d6064df7f13cb33e25113a
Travis Donia <tdonia@gmail.com>
2023-05-30 16:04:01 -0400

    readd react rollup plugin

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// plugins: [react()],
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: 'src/ModelModelWidget.jsx',
-      name: 'ModelModelWidget'
-    },
-    rollupOptions: {
-      // Ensure external packages are not bundled
-      external: ['react'],
+    plugins: [react()],
+    build: {
+        lib: {
+            entry: 'src/ModelModelWidget.jsx',
+            name: 'ModelModelWidget'
+        },
+        rollupOptions: {
+            // Ensure external packages are not bundled
+            external: ['react'],
       output: {
         globals: {
           react: 'React'
